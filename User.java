2fb91673cece2e025bdd4f22db00aefa2b052958
NotME <152592660+guy4581@users.noreply.github.com>
2024-12-18 16:06:26 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,19 +1,17 @@
 import java.time.LocalDate;
 
 public class User {
-    private String name;
-    private LocalDate dob ;
+    protected String name;
+    protected LocalDate dob ;
 
     public User(String name ,int year,int month ,int day){
         this.name = name;
         this.dob = LocalDate.of(year,month ,day);
     }
 
-    public String getName(){return this.name;}
-    public LocalDate getDob(){return this.dob;}
     public void displayInfo(){
-        System.out.println("Username : " + getName());
-        System.out.println("Date of Birth : " + getDob());
+        System.out.println("Username : " + this.name);
+        System.out.println("Date of Birth : " + this.dob);
 
     }
 
